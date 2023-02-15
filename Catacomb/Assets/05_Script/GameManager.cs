406f684a4568bf8fba2662634142f4b70971c0aa
Porinu <66125691+Porinu@users.noreply.github.com>
2023-02-15 23:31:11 +0100

    Ajouts liés aux orbes
    -Ajout de dossiers
    -Prefab prototype d'orbe
    -GameManager avec PV et Quantité d'orbes
    -Script recolte orbes
    -Textures & matériaux pour les orbes
    -ébauche du FX pour l'obtention d'une orbe (voir même d'autres objets?)

---
+++ Catacomb/Assets/05_Script/GameManager.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static int PV = 5;
+    public static int Orbes = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PV = 5;
+        Orbes = 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Catacomb/Assets/05_Script/S_Orbes.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class S_Orbes : MonoBehaviour
+{
+    /* On va demander au script que quand on touche le trigger de l'orbe, ajout de x unité à la variable Orbes, fait appaitre un fx lumineux
+     * pour dire qu'on a bien touché l'orbe puis le GameObject se détruise. 
+     * Il faudra que le script écrive bien le changement de valeur de Orbes dans l'UI fait à cet effet.*/
+
+    public int QuantiteOrbe;
+
+    public GameObject PrefabOrbe;
+    public GameObject FX;
+    public GameObject ValeurOrbes;
+
+    void Start()
+    {
+        ValeurOrbes = GameObject.Find("QuantitéOrbes");
+        ValeurOrbes.GetComponent<Text>().text = GameManager.Orbes.ToString();
+    }
+
+
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Orbe touchée");
+        Instantiate(FX,this.transform.position,Quaternion.identity);
+        GameManager.Orbes = GameManager.Orbes + QuantiteOrbe;
+        ValeurOrbes.GetComponent<Text>().text = GameManager.Orbes.ToString();
+        Destroy(PrefabOrbe,0.1f);
+    }
+
+}
