7a36a2ecfe37a51ded1d5c19181005664dc0a0bf
Porinu <66125691+Porinu@users.noreply.github.com>
2023-02-22 00:01:55 +0100

    Update Orbes/ControlerPerso
    Prefab des Orbes de valeurs différentes = ok
    Prefab L.Boy = ok
    
    Script PlayerCTLR:
    -Deplacement= ok
    -Saut = ok
    -Anim' =à moitié fait
    -Input = à moitié fait
    -Climbing = pas au point
    -Course = non

--- Catacomb/Assets/05_Script/GameManager.cs
+++ Catacomb/Assets/05_Script/GameManager.cs
@@ -5,18 +5,7 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     public static int PV = 5;
-    public static int Orbes = 1;
+    public static int Orbes = 0;
+    public static bool isClimbing = false;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        PV = 5;
-        Orbes = 1;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
--- Catacomb/Assets/05_Script/S_PlayerControler.cs
+++ Catacomb/Assets/05_Script/S_PlayerControler.cs
@@ -6,27 +6,48 @@ public class S_PlayerControler : MonoBehaviour
 {
 
     private CharacterController PlayerCTRL;
+    private Animator AnimationCTRL;
 
     public float PlayerSpeed, JumpSpeed;
-    private float AxisH, DeplacementY; 
+    private float AxisH, AxisV, DeplacementY; 
     public float Gravity;
 
     private Vector3 Deplacement;
 
-    private bool isOnGround;
+    private bool isOnGround, isRunning;
+    private bool Flag_Climb;
 
 
     void Start()
     {
         PlayerCTRL = GetComponent<CharacterController>();
+        AnimationCTRL = GetComponent<Animator>();
+        Flag_Climb = false;
 
     }
 
     void Update()
     {
         AxisH = Input.GetAxis("Horizontal");
+        AxisV = Input.GetAxis("Vertical");
+        AnimationCTRL.SetFloat("AxisH", AxisH);
+        AnimationCTRL.SetFloat("AxisV", AxisV);
+
+        AnimationCTRL.SetBool("isOnGround", isOnGround);
+        AnimationCTRL.SetBool("isClimbing", GameManager.isClimbing);
+        AnimationCTRL.SetBool("isRunning", isRunning);
+
 
         isOnGround = PlayerCTRL.isGrounded;
+
+
+
+        if (Input.GetAxis("Fire1") >= 0.9f)
+        {
+            Flag_Climb = !Flag_Climb;
+            AnimationCTRL.SetTrigger("Climb");
+        }
+
 
         if (Input.GetAxis("Jump") >= 0.1f) DeplacementY = JumpSpeed;
 
@@ -35,13 +56,33 @@ public class S_PlayerControler : MonoBehaviour
         Deplacement = this.transform.TransformDirection(Deplacement);
         Deplacement = Deplacement * PlayerSpeed;
 
+
         //Setup pour faire sauter le personnage
         DeplacementY = DeplacementY - Gravity * Time.deltaTime;
         if (DeplacementY <= -1.0f) DeplacementY = -1.0f;
         Physics.SyncTransforms();
         Deplacement.y = DeplacementY;
 
-        PlayerCTRL.Move(Deplacement * Time.deltaTime);  //Fait bouger le personnage selon les parametres de transform setup au dessus
 
+        if (Flag_Climb == true)
+        {
+            GameManager.isClimbing = true;
+            Gravity = 0;
+            AnimationCTRL.applyRootMotion = false;
+
+            Deplacement = new Vector3(0, AxisV, 0);
+            Deplacement = transform.TransformDirection(Deplacement); //Calcul le Deplacement relatif au personnage et non pas au world
+            Deplacement = Deplacement * PlayerSpeed;
+ 
+        }
+        else
+        {
+            GameManager.isClimbing = false;
+            Gravity = 9.8f;
+            AnimationCTRL.applyRootMotion = true;
+        }
+
+
+        PlayerCTRL.Move(Deplacement * Time.deltaTime); //Fait bouger le personnage selon les parametres de transform setup au dessus
     }
 }
