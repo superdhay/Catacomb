85fc956ecbf5c815bd6d3cf5800daa60d8f1a729
Sasha Dhaeyer <sashadhaeyer@gmail.com>
2023-02-27 12:54:03 +0100

    05-Script : AI rotate when it arrivfes at the ends of parkour. AI player detection. Attack in WIP

---
+++ Catacomb/Assets/05_Script/AI/S_SmallSpider.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class S_SmallSpider : S_Enemy
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        base.Start();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        base.Update();
+    }
+
+    public override void AttackPlayer(Vector3 playerPosition)
+    {
+        Debug.Log("Toucher");
+    }
+}
