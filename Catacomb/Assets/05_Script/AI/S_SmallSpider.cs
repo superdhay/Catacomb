626eda2fb295022ecaf7bdebc00aa454d12e6d83
Sasha Dhaeyer <sashadhaeyer@gmail.com>
2023-03-09 13:38:07 +0100

    05-Script/AI : Little spider done + Prefab creation.

--- Catacomb/Assets/05_Script/AI/S_SmallSpider.cs
+++ Catacomb/Assets/05_Script/AI/S_SmallSpider.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class S_SmallSpider : S_Enemy
 {
-    
+
     // Start is called before the first frame update
     public void Start()
     {
@@ -17,9 +17,39 @@ public class S_SmallSpider : S_Enemy
         base.Update();
     }
 
+    /*
+     * Function that will manage enemy attack.
+     * Override function from S_Enemy.
+     * 
+     * @param playerPosition (Vector3) : the position of the player when he was hit by the detection ray.
+     */
     public override void AttackPlayer(Vector3 playerPosition)
     {
         Debug.Log("Toucher");
+
+        //Move Enemy from current waypoint to the next one using MoveTowards method.
+        transform.position = Vector3.MoveTowards(
+            transform.position,
+            playerPosition,
+            (base.GetMoveSpeed() + 1f) * Time.deltaTime
+        );
+        base.Animator.SetBool("IsMoving", true);
+
+        if (transform.position.z == playerPosition.z)
+        {
+            base.Animator.SetBool("IsMoving", false);
+            base.Animator.SetBool("IsAttacking", true);
+        }
     }
-    
+
+    //Function that manage the collision.
+    private void OnTriggerEnter(Collider collision)
+    {
+        //Verify if it is the player that is hit.
+        if (collision.tag == "Player")
+        {
+            Debug.Log("hit");
+            GameManager.PV--;
+        }
+    }
 }
